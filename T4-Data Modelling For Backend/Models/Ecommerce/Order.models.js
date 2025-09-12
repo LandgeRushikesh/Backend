8a402c2fb09e4dc99d8b068d3ae7c46562ccd138
LandgeRushikesh <rushilandge12@gmail.com>
2025-09-12 15:22:03 +0530

    feat: add models for Category, Product, Order, and User in the Ecommerce module

---
+++ T4-Data Modelling For Backend/Models/Ecommerce/Order.models.js
@@ -0,0 +1,39 @@
+import mongoose, { mongo } from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    Customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    OrderItem: {
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERED"],
+        default: "PENDING"
+    }
+
+}, { timestamps: true })
+
+export const Order = mongoose.model("Order", orderSchema)
