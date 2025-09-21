80149ed23f76325e7b8edb652dec9f73d27d019e
LandgeRushikesh <rushilandge12@gmail.com>
2025-09-21 14:05:33 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes for error handling and response management

---
+++ T5-Backend Project/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class APiResponse {
+    constructor(statusCode, message = "Success", data) {
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+        this.success = statusCode < 400
+    }
+}
+
+export { APiResponse }
---
+++ T5-Backend Project/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// First Approach
+
+const asyncHandler = async (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+// Second Approach
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) =>{}
+// const asyncHandler = (func) =>{
+//     async () => {}
+// }
+
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
